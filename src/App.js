b6c0a60d000084415aca1ca49e7f78269fc828aa
tvinnie <tvinnie77@gmail.com>
2022-10-17 07:23:27 +0300

    Created a dashboard component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from './components/Pages/Home';
 import PageWrapper from './components/PageWrapper';
-import { BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Routes, Switch} from 'react-router-dom';
 import About from './components/Pages/About';
 import Services from './components/Common/Services';
 import Portfolio from './components/Common/Portfolio';
@@ -9,7 +9,13 @@ import Team from './components/Common/Team';
 import AdminWrapper from './components/AdminWrapper';
 import Login from './components/Pages/Login';
 
+import Dashboard from './components/Pages/Dashboard';
+
+import {connect} from 'react-redux';
+import { useState } from 'react';
+
 function App() {
+  const [loggedIn] = useState(true)
   return (
     <Router>
       
@@ -17,10 +23,19 @@ function App() {
      
         <Route
           path="/admin"
-          element = { 
-            <AdminWrapper>
-              <Login />
+          element = {
+              <AdminWrapper>
+              
+                <Login />
+              <RequireAuth>
+                <Dashboard />
+              </RequireAuth>
+                {/* <Dashboard />   */}
+              
+             
+              
             </AdminWrapper>
+           
           }
         />  
 
@@ -80,9 +95,24 @@ function App() {
 
 
         </Routes>
-      {/* </PageWrapper> */}
+    
    </Router>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    auth: state.auth,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/components/Pages/AboutUs.js
+++ src/components/Pages/AboutUs.js
@@ -1,18 +1,11 @@
 import React, {Component} from "react";
-import Header from "../Common/Header";
 import Timeline from "../Common/Timeline";
-import about from "../assets/img/another.png";
 
 class About extends Component{
     render(){
         return(
             <div>
-                {/* <Header
-                title="About Us"
-                subtitle="It's really a great story"
-                showButton={false}
-                image={about}
-                /> */}
+                
 
         <section className="page-section" id="about">
             <div className="container">
